0a547a87adeca1385ee3050ae0031e2ff026134e
Vuvantuan1122 <112723905+Vuvantuan1122@users.noreply.github.com>
2025-08-17 21:18:39 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -18,10 +18,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",   // tạm thời cho tất cả (hoặc thay bằng domain Render của bạn)
     methods: ["GET", "POST"]
   }
 });
+
 
 const PORT = process.env.PORT || 3000;
 
